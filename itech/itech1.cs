b71c0ae06cc839943c4c46c2d26454cc6415d5cd
emmanuel9108 <73707326+emmanuel9108@users.noreply.github.com>
2020-10-30 23:42:03 +0800

    v001

---
+++ itech/itech1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace itech
+{
+    public partial class iTech1 : Form
+    {
+        public iTech1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
