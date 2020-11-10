696d105b7edf7331592098015f0b16b69b3e68de
Emmanuel <emmanuel9108@gmail.com>
2020-11-10 14:28:31 -0800

    v.14 se crean formularios para las carpetas

---
+++ itech/Compras/Entradas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iTechERP.Compras
+{
+    public partial class Entradas : Form
+    {
+        public Entradas()
+        {
+            InitializeComponent();
+        }
+    }
+}
